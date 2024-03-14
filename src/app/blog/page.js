a33aa750c863a1f6f55c81d49a5246253684d599
Katsiaryna Voronich <katarinka403@gmail.com>
2024-03-14 12:14:48 +0300

    init: start project

---
+++ src/app/blog/page.js
@@ -0,0 +1,15 @@
+export default function Blog() {
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
+        <h1>Blog Woman in Tech</h1>
+        <div>
+          <h3>Запущен новый курс по Next.js в рамказ нашего проекта</h3>
+        </div>
+        <div>
+          <h3>Отзывы на Next.js курс великлепные</h3>
+        </div>
+      </div>
+    </main>
+  );
+}
