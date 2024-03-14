eb871b64dccd1ad4517dfa1585588444d3743e08
Katsiaryna Voronich <katarinka403@gmail.com>
2024-03-14 12:40:48 +0300

    feat: add links to pages

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,15 +1,22 @@
+import Link from 'next/link';
+
 export default function Blog() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
-        <h1>Blog Woman in Tech</h1>
-        <div>
-          <h3>Запущен новый курс по Next.js в рамказ нашего проекта</h3>
+    <>
+      <header className='flex items-center justify-center py-8 text-xl'>
+        <Link href='/'>Main</Link>
+      </header>
+      <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+        <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
+          <h1>Blog Woman in Tech</h1>
+          <div>
+            <h3>Запущен новый курс по Next.js в рамказ нашего проекта</h3>
+          </div>
+          <div>
+            <h3>Отзывы на Next.js курс великлепные</h3>
+          </div>
         </div>
-        <div>
-          <h3>Отзывы на Next.js курс великлепные</h3>
-        </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
